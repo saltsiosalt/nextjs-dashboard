d0ac10f40697f1377f9e4d79dbc4b6a59b6d74f7
saltsiosalt <20432241+saltsiosalt@users.noreply.github.com>
2023-11-06 00:23:21 +0900

    Chapter5まで

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css'
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* <body>{children}</body> */}
+      {/* <body>要素にInterを追加することで、アプリケーション全体にフォントが適用されます。
+      ここでは、フォントを滑らかにする Tailwind antialiased クラスも追加しています。
+      このクラスを使用する必要はありませんが、フォントに良いタッチを加えます。 */}
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
