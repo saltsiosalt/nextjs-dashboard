1adca135c77b27f5aadaf645a139b16f93b8e848
saltsiosalt <20432241+saltsiosalt@users.noreply.github.com>
2023-11-26 19:11:47 +0900

    chapter11 まで終了

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,46 @@
-export default function Page() {
-  return <p>Invoices Page</p>;
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
+
+// 検索機能はクライアントとサーバーにまたがります。
+// ユーザーがクライアント上で請求書を検索すると、URLパラメータが更新され、
+// サーバー上でデータが取得され、新しいデータでテーブルがサーバー上で再レンダリングされます。
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchInvoicesPages(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        {/* `<Search/>`はユーザーが特定の請求書を検索できるようにします。 */}
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      {/* <Table/>は請求書を表示します。 */}
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination/>は、ユーザーが請求書のページ間を移動できるようにします。 */}
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
