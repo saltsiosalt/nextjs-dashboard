c2163b76e0503c1547a4f5cf34ace52302382212
saltsiosalt <20432241+saltsiosalt@users.noreply.github.com>
2023-12-28 15:20:27 +0900

    chapter14まで終了

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,10 +5,15 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
-  id: z.string(),
+  id: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
   customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  amount: z.coerce.number()
+  .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'],{
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
@@ -17,21 +22,44 @@ const FormSchema = z.object({
  */
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+// これは @types/react-dom が更新されるまでの一時的なものです
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+  // Zodを使ってフォーム・フィールドを検証する
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  // フォームのバリデーションに失敗した場合は、早期にエラーを返す。そうでなければ続行する。
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+  // データベースに挿入するデータを準備する
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
+  // データベースにデータを挿入する
   try {
     await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
       `;
   } catch (error) {
+    // データベースエラーが発生した場合は、より具体的なエラーを返します。
     return {
       message: 'Database Error: Failed to Create Invoice.',
     };
@@ -47,12 +75,25 @@ export async function createInvoice(formData: FormData) {
 // Zod を使用して期待されるタイプを更新する
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
 
   const amountInCents = amount * 100;
 
--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -15,12 +15,17 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const primary = {
+    light : "#fff",
+    red: "#ffad66",
+  }
+
   const {totalPaidInvoices, totalPendingInvoices, numberOfInvoices,numberOfCustomers} = await fetchCardData();
   return (
     <>
       {/* NOTE: コースのこのポイントまで来たら、このコードをコメントすること。 */}
 
-      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices}  type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
